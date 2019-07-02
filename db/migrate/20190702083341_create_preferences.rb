8d0de4f42dd11bffc196cbc28c8e3d4822daefb5
Laura Felce <crepu83@yahoo.it>
2019-07-02 08:35:29 +0000

    Automatically backed up by Learn

--- db/migrate/20190702083341_create_preferences.rb
+++ db/migrate/20190702083341_create_preferences.rb
@@ -2,6 +2,9 @@ class CreatePreferences < ActiveRecord::Migration[5.2]
   def change
     create_table :preferences do |t|
       t.string :artist_sort_order
+      t.string :song_sort_order
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
 
       t.timestamps
     end
