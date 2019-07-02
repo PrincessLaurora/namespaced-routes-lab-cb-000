ec296e318cfff9d93030337e82c71c75ef58a797
Laura Felce <crepu83@yahoo.it>
2019-07-02 09:29:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,8 @@ class SongsController < ApplicationController
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
+      elsif @preference && @preference.song_sort_order
+        @songs = @artist.songs.order(title: @preference.song_sort_order)  
       else
         @songs = @artist.songs
       end
