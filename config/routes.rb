96d7808c6fb2b2bc9de01030d902c557ead23060
Laura Felce <crepu83@yahoo.it>
2019-07-02 08:48:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :preferences
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end  
   resources :artists do
     resources :songs, only: [:index, :show]
   end
