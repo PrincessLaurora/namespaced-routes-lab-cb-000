f37b1ed3c3a4d3b713d01149fd6429f215ca804a
Laura Felce <crepu83@yahoo.it>
2019-07-02 09:18:32 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,7 +10,7 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @preference = Preference.first
+    @preferences = Preference.first
     if @preference.allow_create_artists?
       @artist = Artist.new
     else
